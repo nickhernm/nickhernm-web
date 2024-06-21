9d6af65e79d30e5382eb44c40191393ada78f86a
nickhernm <ramajahde@gmail.com>
2024-06-21 16:45:04 +0200

    Initial

---
+++ proyecto-cv-react/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>Jaime Hernández</h1>
+      <p>Computer Engineering Student</p>
+    </header>
+  );
+}
+
+export default Header;
